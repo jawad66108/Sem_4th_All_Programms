76a4fef6f1fe5a7426e4ddc4691ee2a10ff6d1d9
Jawad Hussain <jawad66108@gmail.com>
2026-05-13 23:13:31 +0500

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,47 @@
+# Inside the editor, complete the following steps:
+# Create a variable txt with the value "Hello, World!"
+# Print the characters from index 2 to 5 (slicing)
+# Print txt converted to upper case
+# Create a variable name with the value "Python"
+# Use an f-string to print "I love Python" using the name variable
+
+# # Create the variable
+# x= "Hello, World"
+# # Print characters from index 2 to 5
+# print(x[2:5])
+# # Print in upper case
+# print(x.upper())
+# # Create the name variable
+# Python = "I love Python"
+
+
+# Q1 — Second Largest
+
+# Given n integers, print the second largest distinct number.
+
+# Input
+# 5
+# 1 9 2 9 7
+# Output
+# 7
+# print(len(num))
+# nn_um=sorted(num)
+# print(nn_um)
+# num =[1,9,2,9,7]
+
+# max_d=0
+# for x in range(len(num)-1):
+#     n = num[x] - num[x+1]
+#     if n>max_d:
+#         max_d=n
+
+# print(max_d)
+
+
+# st = "maaom"
+# new_st=st[::-1]
+# if new_st == st:
+#     print("It is")
+# else:
+#     print("Its not ")
+
